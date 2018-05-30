e5b571c3a06b839a59674e7c8a87751b110f95c1
Chris Casey <cdcasey@gmail.com>
2018-05-30 10:19:37 -0500

    Reds can be adjusted

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 document.getElementById("scream").onload = function () {
     const desaturateButton = document.getElementById('desaturate');
-    desaturateButton.addEventListener('click', desaturate);
+    const redSlider = document.getElementById('red');
+    const redDisplay = document.getElementById('red-val');
+    redDisplay.innerText = redSlider.value;
 
+    desaturateButton.addEventListener('click', desaturate);
     let c = document.getElementById("myCanvas");
     let img = document.getElementById("scream");
     c.width = img.width / 2;
@@ -11,7 +14,36 @@ document.getElementById("scream").onload = function () {
         0, 0, c.width, c.height);
 
     let imgData = ctx.getImageData(0, 0, c.width, c.height);
-    console.log(imgData.width, imgData.height);
+    let reds = [];
+    let greens = [];
+    let blues = [];
+    for (let i = 0; i < imgData.data.length; i += 4) {
+        let red = imgData.data[i];
+        let green = imgData.data[i + 1];
+        let blue = imgData.data[i + 2];
+        if (red > green && red > blue) {
+            reds.push(i)
+        } else if (green > red && green > blue) {
+            greens.push(i + 1)
+        } else if (blue > red && blue > green) {
+            blues.push(i + 2)
+        }
+    }
+    redSlider.oninput = function () {
+        redDisplay.innerHTML = this.value;
+
+        for (let i = 0; i < reds.length; i++) {
+            const red = reds[i];
+            if (this.value > 0) {
+                imgData.data[red] += (255 - imgData.data[red]) * (this.value / 100);
+            } else {
+                imgData.data[red] -= imgData.data[red] * Math.abs(this.value / 100);
+            }
+        }
+        ctx.putImageData(imgData, 0, 0);
+        // desaturate();
+    }
+    // desaturate();
     // imgData = desaturate(ctx);
     // invert colors
     // let i;
@@ -45,7 +77,6 @@ document.getElementById("scream").onload = function () {
         imgData.data = removeColor(imgData.data);
         ctx.putImageData(imgData, 0, 0);
     }
-    ctx.putImageData(imgData, 0, 0);
     function removeColor(data) {
         for (let i = 0; i < data.length; i += 4) {
             // console.log(imgData);
@@ -53,11 +84,22 @@ document.getElementById("scream").onload = function () {
             let green = data[i + 1];
             let blue = data[i + 2];
             let alpha = data[i + 3];
-            data[i] = red * .2125 + green * .7154 + blue * .0721;
-            data[i + 1] = red * .2125 + green * .7154 + blue * .0721;
-            data[i + 2] = red * .2125 + green * .7154 + blue * .0721;
+            const avg = (red + green + blue) / 3;
+            // const avg = red * .2125 + green * .7154 + blue * .0721;
+            data[i] = data[i + 1] = data[i + 2] = avg;
+            // data[i] = red * .2125 + green * .7154 + blue * .0721;
+            // data[i + 1] = red * .2125 + green * .7154 + blue * .0721;
+            // data[i + 2] = red * .2125 + green * .7154 + blue * .0721;
         }
         return data;
     }
+
+    for (let i = 0; i < imgData.data.length; i += 4) {
+        const element = imgData.data[i];
+        // imgData.data[i + 3] = element * 2;
+    }
+    console.log(imgData);
+
+    ctx.putImageData(imgData, 0, 0);
 };
 
